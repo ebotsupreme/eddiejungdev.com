d25325166bc40649f626b9f8a20f7a84d4f93c83
ebotsupreme <ejung716@gmail.com>
2025-06-04 07:56:46 -0700

    Setup basice structure for experience page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import About from "./about/page";
+import Experience from "./experience/page";
 
 const Home = () => {
   return (
     <div>
       <section id="about">
         <About />
+      </section>
+      <section id="experience">
+        <Experience />
       </section>
     </div>
   );
---
+++ src/app/ui/experience/myExperience.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const MyExperience = () => {
+  return (
+    <div>
+      <p>My Experience</p>
+    </div>
+  );
+};
+
+export default MyExperience;
