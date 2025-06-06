4c34147f60b3d246888c3755017187a95fdc4c0d
ebotsupreme <ejung716@gmail.com>
2025-06-06 12:50:58 -0700

    Add transition for hover

--- src/app/ui/experience/resumeInfo.tsx
+++ src/app/ui/experience/resumeInfo.tsx
@@ -12,7 +12,7 @@ const ResumeInfo = () => {
         {resumeData.map((data, index) => (
           <li key={index} className="mb-12">
             <div className="group relative grid items-start pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div className="absolute -inset-x-4 -inset-y-4 lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+              <div className="absolute -inset-x-4 -inset-y-4 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
               <header
                 className="mt-1 mb-2 text-xs font-semibold uppercase text-slate-500 sm:col-span-2 tracking-wide"
                 aria-label={data.date}
@@ -26,7 +26,7 @@ const ResumeInfo = () => {
                       href={data.href}
                       target="_blank"
                       aria-label={data.ariaLabel}
-                      className="inline-flex items-baseline group/link font-medium text-base leading-tight text-shadow-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link"
+                      className="inline-flex items-baseline group/link font-medium text-base leading-tight text-slate-200 text-shadow-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link"
                     >
                       <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
                       <span>
