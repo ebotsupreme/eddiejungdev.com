cd1916bea2e0a3c86ac0159395273e93e9aa868c
ebotsupreme <ejung716@gmail.com>
2025-06-06 10:49:29 -0700

    Make svg icon reusable.

--- src/app/ui/experience/resumeInfo.tsx
+++ src/app/ui/experience/resumeInfo.tsx
@@ -3,6 +3,7 @@
 import { resumeData } from "@/app/lib/resume-data";
 import Link from "next/link";
 import Pill from "../pill/pill";
+import ArrowUpRightIcon from "../icons/ArrowUpRightIcon";
 
 const ResumeInfo = () => {
   return (
@@ -32,19 +33,7 @@ const ResumeInfo = () => {
                         {data.title} ·{" "}
                         <span className="inline-block">
                           {data.ariaLabel}
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              clipRule="evenodd"
-                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                            />
-                          </svg>
+                          <ArrowUpRightIcon />
                         </span>
                       </span>
                     </Link>
@@ -76,19 +65,7 @@ const ResumeInfo = () => {
             View Full{" "}
             <span className="inline-block">
               Resume
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                />
-              </svg>
+              <ArrowUpRightIcon />
             </span>
           </span>
         </Link>
