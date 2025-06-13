386503ba73782e0532c11e68bda7fbe41e059948
ebotsupreme <ejung716@gmail.com>
2025-06-13 15:16:14 -0700

    Refactor teddy component.

---
+++ src/app/lib/teddy-data.ts
@@ -0,0 +1,29 @@
+import { TeddyData } from "./definitions";
+
+export const teddyData: TeddyData[] = [
+  {
+    color: "text-[#DBE0E5ff]",
+    delay: "delay-[50ms]",
+    character: "T",
+  },
+  {
+    color: "text-[#DAD7D4ff]",
+    delay: "delay-[75ms]",
+    character: "e",
+  },
+  {
+    color: "text-[#CFBFB4ff]",
+    delay: "delay-[100ms]",
+    character: "d",
+  },
+  {
+    color: "text-[#A7A09Bff]",
+    delay: "delay-[125ms]",
+    character: "d",
+  },
+  {
+    color: "text-[#70625Dff]",
+    delay: "delay-[150ms]",
+    character: "y",
+  },
+];
